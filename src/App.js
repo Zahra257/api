b77f021532f3354cab0cde52044642241d073cce
zahra257 <najemzahra3@gmail.com>
2021-08-13 01:37:31 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Listofuser from './Listofuser'
+// import Maps from './Maps'
+import './App.css'
+
+const App = () =>{
+   return (
+    <div className='map'>
+      <Listofuser/>
+      {/* <Maps/> */}
+    </div>
+)
+}
+
+export default App;
